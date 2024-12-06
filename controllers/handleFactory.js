51286fededbc8022b5b924612fa74875309da193
Girmay Addisu <emmanda2050@gmail.com>
2024-12-06 15:10:39 +0300

    factory on getting data

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     const deltedData = await Model.findByIdAndDelete(req.params.id);
@@ -40,6 +41,49 @@ exports.createOne = Model => catchAsync(async (req, res, next) => {
         status: 'success',
         data: {
             data: doc
+        }
+    });
+});
+
+
+exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+
+    if (!doc) {
+        return next(new AppError('No doc found with this id', 404));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        data: {
+            data: doc
+        }
+    });
+});
+
+
+exports.getAll = Model => catchAsync(async (req, res, next) => {
+
+    //to allow nested route review on tour 
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const features = new APIFeatures(Model.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .pagination()
+    const docs = await features.query;
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: docs.length,
+        data: {
+            data: docs
         }
     });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -10,18 +10,6 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
 
 exports.updateMe = catchAsync(async (req, res, next) => {
 
@@ -53,20 +41,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet found! please use /signup route'
   });
 };
 
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User); // No password update is done here
 exports.deleteUser = factory.deleteOne(User);
